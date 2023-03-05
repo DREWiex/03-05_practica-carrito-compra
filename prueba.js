5de50e63496596f02a7024900903edba58be1613
DREWiex <aleon88@gmail.com>
2023-03-05 19:35:50 +0100

    Última act. antes de la entrega

--- prueba.js
+++
@@ -1,8 +0,0 @@
-const objCosas = [
-    {nombre: 'Pepe', edad: 20},
-    {nombre: 'José', edad: 25},
-    {nombre: 'Ana', edad: 20},
-];
-
-const cosas = [...new Set(objCosas.map((item) => item.edad))];
-console.log(cosas);
